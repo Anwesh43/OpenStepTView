dc652c13982d41fb4cf2570f87304f6f6ca9ed45
Anwesh43 <anweshthecool0@gmail.com>
2020-02-08 13:05:37 +0530

    adding OBTNode class

--- openbouncytview/src/main/java/com/anwesh/uiprojects/openbouncytview/OpenBouncyTView.kt
+++ openbouncytview/src/main/java/com/anwesh/uiprojects/openbouncytview/OpenBouncyTView.kt
@@ -124,4 +124,46 @@ class OpenBouncyTView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class OBTNode(var i : Int, val state : State = State()) {
+
+        private var next : OBTNode? = null
+        private var prev : OBTNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = OBTNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawOBTNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : OBTNode {
+            var curr : OBTNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
