9a4e6036fdcb03650f1bbcad87a65751f197bfab
Anwesh43 <anweshthecool0@gmail.com>
2020-02-08 12:43:17 +0530

    adding constants and libraries

--- openbouncytview/src/main/java/com/anwesh/uiprojects/openbouncytview/OpenBouncyTView.kt
+++ openbouncytview/src/main/java/com/anwesh/uiprojects/openbouncytview/OpenBouncyTView.kt
@@ -4,3 +4,19 @@ package com.anwesh.uiprojects.openbouncytview
  * Created by anweshmishra on 08/02/20.
  */
 
+import android.view.MotionEvent
+import android.view.View
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.02f / lines
+val delay : Long = 20
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
+
