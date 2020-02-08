74fb13e0907be9a073429b0486b4976c490a4c78
Anwesh43 <anweshthecool0@gmail.com>
2020-02-08 13:12:33 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedopenbouncytview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedopenbouncytview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedopenbouncytview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.openbouncytview.OpenBouncyTView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        OpenBouncyTView.create(this)
     }
 }
